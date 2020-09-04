d75e444569b962826688fb44ac6796c10488a98a
skq <1109984188@qq.com>
2020-09-04 20:56:57 +0800

    完成了index页面

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,50 +1,113 @@
 $(function () {
-    var form = layui.form
-    var layer = layui.layer
-    form.verify({
-        pwd: [/^[\S]{6,12}$/, "密码必须6-12位且不能出现空格"],
-        repwd: function (value) {
-            // 通过形参拿到的是确认密码框中的内容
-            // 还需要拿到密码框中的内容
-            // 然后进行一次等于的判断
-            // 如果判断失败,则return一个提示消息即可
-            var pwd = $('.reg-box [name=password]').val()
-            if (pwd !== value) {
-                return '两次密码不一致！'
-            }
-        }
-    })
-    $("#form_reg").on("submit", function (e) {
-        e.preventDefault()
-        var data = {
-            username: $("#form_reg [name=username]").val(),
-            password: $("#form_reg [name=password]").val()
-        }
-        $.post("http://ajax.frontend.itheima.net/api/reguser", data, function (res) {
-            if (res.status !== 0) {
-                return layer.msg(res.message)
-            }
-            else {
-                layer.msg(res.message)
-                $("#form_login")[0].reset()
-                $("#link_login").click()
-            }
-        })
-    })
-    $("#form_login").on("submit", function (e) {
-        e.preventDefault()
-        var data = $(this).serialize()
-        $.post("http://ajax.frontend.itheima.net/api/login", data, function (res) {
+    getUserInfo()
+})
+// 获取用户信息
+function getUserInfo() {
+    $.ajax({
+        method: "get",
+        url: "/my/userinfo",
+        // headers: {
+        //     Authorization: localStorage.getItem("token")
+        // },
+        success: function (res) {
             console.log(res);
             if (res.status !== 0) {
-                layer.msg(res.message)
-            } else {
-                layer.msg(res.message)
-                localStorage.setItem("token", res.token)
-                $("#form_login")[0].reset()
-                // location.href = "/index.html"
-
+                return layer.msg("获取用户信息失败！")
             }
-        })
+            else {
+                renderAvatar(res.data)
+            }
+        }
     })
+}
+// 渲染头像
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $("#welcome").html("欢迎&nbsp&nbsp" + name)
+
+    if (user.user_pic !== null) {
+        $(".layui-nav-img").prop("src", user.user_pic).show()
+        $(".ext-avatar").hide()
+    } else {
+        $(".text-avatar").html(name[0].toUpperCase()).show()
+        $(".layui-nav-img").prop("src", user.user_pic).hide()
+    }
+
+}
+$("#tuichu").on("click", function () {
+    // console.log(111);
+    var layer = layui.layer
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+        localStorage.removeItem("token")
+        location.href = "/login.html"
+        layer.close(index);
+    });
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function () {
+//     getUserInfo()
+// })
+// function getUserInfo() {
+//     $.ajax({
+//         method: "get",
+//         url: "/my/userinfo",
+//         // headers: {
+//         //     Authorization: localStorage.getItem("token")
+//         // },
+//         success: function (res) {
+//             console.log(res);
+//             if (res.status !== 0) {
+//                 return layui.layer.msg("获取用户信息失败！")
+//             } else {
+//                 renderAvatar(res.data)
+//             }
+//         },
+//         complete: function (res) {
+//             // console.log(res);
+//             if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+//                 localStorage.removeItem("token")
+//                 location.href = "/login.html"
+//             }
+//         }
+//     })
+// }
+// function renderAvatar(user) {
+//     // 1. 获取用户的名称
+//     var name = user.nickname || user.username
+//     // 2. 设置欢迎的文本
+//     $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+//     // 3. 按需渲染用户的头像
+//     if (user.user_pic !== null) {
+//         // 3.1 渲染图片头像
+//         $('.layui-nav-img')
+//             .attr('src', user.user_pic)
+//             .show()
+//         $('.text-avatar').hide()
+//     } else {
+//         // 3.2 渲染文本头像
+//         $('.layui-nav-img').hide()
+//         var first = name[0].toUpperCase()
+//         $('.text-avatar')
+//             .html(first)
+//             .show()
+//     }
+// }
